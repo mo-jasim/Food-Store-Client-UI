368d5bf198971e7d3c52f7514eab3fb092852162
Rakesh K <codersgyan@gmail.com>
2024-05-09 14:01:22 +0300

    create cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import CartItems from './cartItems/cartItems';
 
 const CartPage = () => {
-    return <div>CartPage</div>;
+    return (
+        <section>
+            <div className="container mx-auto py-6">
+                <h1 className="text-lg font-bold">Shopping cart</h1>
+                <div className="bg-white rounded-lg p-6 mt-6">
+                    <CartItems />
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default CartPage;
