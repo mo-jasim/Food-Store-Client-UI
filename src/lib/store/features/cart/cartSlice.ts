40ee0f228cd78ff97e2e1e08ba55290691dd335b
Rakesh K <codersgyan@gmail.com>
2024-05-16 12:54:19 +0300

    remove item from the cart

--- src/lib/store/features/cart/cartSlice.ts
+++ src/lib/store/features/cart/cartSlice.ts
@@ -44,6 +44,12 @@ export const cartSlice = createSlice({
         },
         changeQty: (state, action: PayloadAction<{ hash: string; qty: number }>) => {
             const index = state.cartItems.findIndex((item) => item.hash === action.payload.hash);
+
+            if (action.payload.qty === 0) {
+                state.cartItems.splice(index, 1);
+                window.localStorage.setItem('cartItems', JSON.stringify(state.cartItems));
+                return;
+            }
             // 1 , -1
             // 0 -> 1 = 1
             // 1 -> -1 1 + -1 = 0
