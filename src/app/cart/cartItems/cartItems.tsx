fa1a7d9b188392e658ee12307a24daa42f9b55b4
Rakesh K <codersgyan@gmail.com>
2024-05-16 13:13:50 +0300

    calculate final total

--- src/app/cart/cartItems/cartItems.tsx
+++ src/app/cart/cartItems/cartItems.tsx
@@ -6,6 +6,7 @@ import { useAppSelector } from '@/lib/store/hooks';
 import { ArrowRight, ShoppingCart } from 'lucide-react';
 import { useSearchParams } from 'next/navigation';
 import { Button } from '@/components/ui/button';
+import { getItemTotal } from '@/lib/utils';
 
 const CartItems = () => {
     const searchParams = useSearchParams();
@@ -14,8 +15,14 @@ const CartItems = () => {
     useEffect(() => {
         setIsClient(true);
     }, []);
-
     const cart = useAppSelector((state) => state.cart.cartItems);
+
+    const finalTotal = React.useMemo(() => {
+        return cart.reduce((acc, curr) => {
+            return acc + curr.qty * getItemTotal(curr);
+        }, 0);
+    }, [cart]);
+
     if (!isClient) {
         return null;
     }
@@ -42,7 +49,7 @@ const CartItems = () => {
                 <CartItem key={cartItem.hash} item={cartItem} />
             ))}
             <div className="flex justify-between items-center">
-                <span className="font-bold text-xl">&#8377;{4000}</span>
+                <span className="font-bold text-xl">&#8377;{finalTotal}</span>
                 <Button>
                     Checkout
                     <ArrowRight size={16} className="ml-2" />
