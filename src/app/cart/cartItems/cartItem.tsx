11f52484eb0ecd96d67648a6b38902fc9d63d56c
Rakesh K <codersgyan@gmail.com>
2024-05-16 13:04:32 +0300

    calculate item total price

--- src/app/cart/cartItems/cartItem.tsx
+++ src/app/cart/cartItems/cartItem.tsx
@@ -4,9 +4,11 @@ import QtyChanger from './qtyChanger';
 import { changeQty, CartItem as Item } from '@/lib/store/features/cart/cartSlice';
 import { X } from 'lucide-react';
 import { useAppDispatch } from '@/lib/store/hooks';
+import { useTotal } from '@/lib/hooks/useTotal';
 
 const CartItem = ({ item }: { item: Item }) => {
     const dispatch = useAppDispatch();
+    const total = useTotal(item);
 
     return (
         <>
@@ -39,7 +41,7 @@ const CartItem = ({ item }: { item: Item }) => {
                         </QtyChanger>
                     </div>
                     <div className="flex">
-                        <div className="font-bold w-12">&#8377;300</div>
+                        <div className="font-bold w-12">&#8377;{total * item.qty}</div>
                         <button
                             className="ml-4"
                             onClick={() => {
---
+++ src/lib/hooks/useTotal.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { CartItem } from '../store/features/cart/cartSlice';
+
+export function useTotal(product: CartItem) {
+    const totalPrice = React.useMemo(() => {
+        const toppingsTotal = product.chosenConfiguration.selectedToppings.reduce(
+            (acc, curr) => acc + curr.price,
+            0
+        );
+
+        const configPricing = Object.entries(product.chosenConfiguration.priceConfiguration).reduce(
+            (acc, [key, value]: [string, string]) => {
+                const price = product.priceConfiguration[key].availableOptions[value];
+                return acc + price;
+            },
+            0
+        );
+        return configPricing + toppingsTotal;
+    }, [product]);
+    return totalPrice;
+}
