60d0014609b11902326dc0c1ed31748d9094ddb6
Rakesh K <codersgyan@gmail.com>
2024-05-25 16:43:26 +0300

    add session functionality

--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -6,10 +6,13 @@ import { Button } from '../ui/button';
 import { Tenant } from '@/lib/types';
 import dynamic from 'next/dynamic';
 import TenantSelect from './tenant-select';
+import { getSession } from '@/lib/session';
 
 const CartCounterWithoutSSR = dynamic(() => import('./cart-counter'), { ssr: false });
 
 const Header = async () => {
+    const session = await getSession();
+
     const tenantsResponse = await fetch(`${process.env.BACKEND_URL}/api/auth/tenants?perPage=100`, {
         next: {
             revalidate: 3600, // 1 hour
@@ -59,7 +62,8 @@ const Header = async () => {
                         <Phone />
                         <span>+91 9800 098 998</span>
                     </div>
-                    <Button size={'sm'}>Logout</Button>
+
+                    <Button size={'sm'}>{session ? 'Logout' : 'Login'}</Button>
                 </div>
             </nav>
         </header>
