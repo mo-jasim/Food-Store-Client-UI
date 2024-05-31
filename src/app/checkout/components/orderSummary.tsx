46c07ed0f324b3c2d441ebafe5f0a48268e916e6
Rakesh K <codersgyan@gmail.com>
2024-05-31 14:35:18 +0300

    add discount with coupon code

--- src/app/checkout/components/orderSummary.tsx
+++ src/app/checkout/components/orderSummary.tsx
@@ -1,8 +1,12 @@
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
+import { verifyCoupon } from '@/lib/http/api';
 import { useAppSelector } from '@/lib/store/hooks';
+import { CouponCodeData } from '@/lib/types';
 import { getItemTotal } from '@/lib/utils';
+import { useMutation } from '@tanstack/react-query';
+import { useSearchParams } from 'next/navigation';
 import React from 'react';
 
 // todo: Move this to the server, and calulate according to your business rules.
@@ -11,9 +15,14 @@ const TAXES_PERCENTAGE = 18;
 const DELIVERY_CHARGES = 100;
 
 const OrderSummary = () => {
+    const searchParam = useSearchParams();
+
     const cart = useAppSelector((state) => state.cart.cartItems);
 
     const [discountPercentage, setDiscountPercentage] = React.useState(0);
+    const [discountError, setDiscountError] = React.useState('');
+
+    const couponCodeRef = React.useRef<HTMLInputElement>(null);
 
     const subTotal = React.useMemo(() => {
         return cart.reduce((acc, curr) => {
@@ -31,9 +40,51 @@ const OrderSummary = () => {
         return Math.round((amountAfterDiscount * TAXES_PERCENTAGE) / 100);
     }, [subTotal, discountAmount]);
 
-    const grandTotal = React.useMemo(() => {
+    const grandWithDiscountTotal = React.useMemo(() => {
         return subTotal - discountAmount + taxesAmount + DELIVERY_CHARGES;
     }, [subTotal, discountAmount, taxesAmount, DELIVERY_CHARGES]);
+
+    const grandWithoutDiscountTotal = React.useMemo(() => {
+        return subTotal + taxesAmount + DELIVERY_CHARGES;
+    }, [subTotal, taxesAmount, DELIVERY_CHARGES]);
+
+    // todo: display error isError, error
+    const { mutate } = useMutation({
+        mutationKey: ['couponCode'],
+        mutationFn: async () => {
+            if (!couponCodeRef.current) {
+                return;
+            }
+
+            const restaurantId = searchParam.get('restaurantId');
+
+            if (!restaurantId) {
+                return;
+            }
+
+            const data: CouponCodeData = {
+                code: couponCodeRef.current.value,
+                tenantId: restaurantId,
+            };
+            return await verifyCoupon(data).then((res) => res.data);
+        },
+        onSuccess: (data) => {
+            if (data.valid) {
+                setDiscountError('');
+                setDiscountPercentage(data.discount);
+                return;
+            }
+
+            setDiscountError('Coupon is invalid');
+            setDiscountPercentage(0);
+        },
+    });
+
+    const handleCouponValidation = (e: React.MouseEvent) => {
+        e.preventDefault();
+
+        mutate();
+    };
 
     return (
         <Card className="w-2/5 border-none h-auto self-start">
@@ -60,11 +111,29 @@ const OrderSummary = () => {
                 <hr />
                 <div className="flex items-center justify-between">
                     <span className="font-bold">Order total</span>
-                    <span className="font-bold">₹{grandTotal}</span>
+                    <span className="font-bold flex flex-col items-end">
+                        <span className={discountPercentage ? 'line-through text-gray-400' : ''}>
+                            ₹{grandWithoutDiscountTotal}
+                        </span>
+                        {discountPercentage ? (
+                            <span className="text-green-700">${grandWithDiscountTotal}</span>
+                        ) : null}
+                    </span>
                 </div>
+                {discountError && <div className="text-red-500">{discountError}</div>}
                 <div className="flex items-center gap-4">
-                    <Input id="fname" type="text" className="w-full" placeholder="Coupon code" />
-                    <Button variant={'outline'}>Apply</Button>
+                    <Input
+                        id="coupon"
+                        name="code"
+                        type="text"
+                        className="w-full"
+                        placeholder="Coupon code"
+                        ref={couponCodeRef}
+                    />
+                    {/* todo: add loading */}
+                    <Button onClick={handleCouponValidation} variant={'outline'}>
+                        Apply
+                    </Button>
                 </div>
 
                 <div className="text-right mt-6">
