386b0cbf57124c87d7c963d24fc2c0b52773bf64
Rakesh K <codersgyan@gmail.com>
2024-06-08 12:38:35 +0300

    clear cart on payment success

--- src/lib/store/features/cart/cartSlice.ts
+++ src/lib/store/features/cart/cartSlice.ts
@@ -60,10 +60,16 @@ export const cartSlice = createSlice({
 
             window.localStorage.setItem('cartItems', JSON.stringify(state.cartItems));
         },
+        clearCart: () => {
+            window.localStorage.setItem('cartItems', JSON.stringify([]));
+            return {
+                cartItems: [],
+            };
+        },
     },
 });
 
 // Action creators are generated for each case reducer function
-export const { addToCart, setInitialCartItems, changeQty } = cartSlice.actions;
+export const { addToCart, setInitialCartItems, changeQty, clearCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
