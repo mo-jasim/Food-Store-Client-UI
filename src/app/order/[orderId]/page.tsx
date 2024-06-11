781b7097773e6ba57c3a1c36c725fdddce8cbd82
Rakesh K <codersgyan@gmail.com>
2024-06-11 14:30:16 +0300

    fetch single order and render

--- src/app/order/[orderId]/page.tsx
+++ src/app/order/[orderId]/page.tsx
@@ -4,8 +4,25 @@ import OrderStatus from './components/orderStatus';
 import { Separator } from '@/components/ui/separator';
 import { Banknote, Coins, LayoutDashboard } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import { cookies } from 'next/headers';
+import { Order } from '@/lib/types';
 
-const SingleOrder = () => {
+const SingleOrder = async ({ params }: { params: { orderId: string } }) => {
+    const response = await fetch(
+        `${process.env.BACKEND_URL}/api/order/orders/${params.orderId}?fields=address,paymentStatus,paymentMode`,
+        {
+            headers: {
+                Authorization: `Bearer ${cookies().get('accessToken')?.value}`,
+            },
+        }
+    );
+
+    if (!response.ok) {
+        console.log(response);
+        throw new Error('Failed to fetch single order');
+    }
+    const order: Order = await response.json();
+
     return (
         <div className="container mt-6 flex flex-col gap-6">
             <Card>
@@ -27,8 +44,10 @@ const SingleOrder = () => {
                     </CardHeader>
                     <Separator />
                     <CardContent className="pt-6">
-                        <h2 className="font-bold">Rakesh K</h2>
-                        <p className="mt-2">55, New Street, upper lane, New Delhi. India. 409876</p>
+                        <h2 className="font-bold">
+                            {order.customerId.firstName + ' ' + order.customerId.lastName}
+                        </h2>
+                        <p className="mt-2">{order.address}</p>
                     </CardContent>
                 </Card>
 
@@ -43,19 +62,19 @@ const SingleOrder = () => {
                         <div className="flex items-center gap-2">
                             <LayoutDashboard size={20} />
                             <h2 className="text-base font-medium">Order reference: </h2>
-                            ord121313123131313
+                            {order._id}
                         </div>
 
                         <div className="flex items-center gap-2 mt-2">
                             <Banknote />
                             <h2 className="text-base font-medium">Payment status: </h2>
-                            <span>Paid</span>
+                            <span>{order.paymentStatus.toUpperCase()}</span>
                         </div>
 
                         <div className="flex items-center gap-2 mt-2">
                             <Coins size={20} />
                             <h2 className="text-base font-medium">Payment method: </h2>
-                            <span>Card</span>
+                            <span>{order.paymentMode.toUpperCase()}</span>
                         </div>
 
                         <Button variant={'destructive'} className="mt-6">
