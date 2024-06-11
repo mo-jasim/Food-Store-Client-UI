d3b0d08dd93f1f22af73224a5d10f74ecf2c4b35
Rakesh K <codersgyan@gmail.com>
2024-06-11 12:01:14 +0300

    show no orders message

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -24,7 +24,7 @@ const Orders = async () => {
         throw new Error('Error Fetching my order.');
     }
 
-    const orders = await response.json();
+    const orders = (await response.json()) || [];
 
     return (
         <div className="container mt-8">
@@ -34,45 +34,53 @@ const Orders = async () => {
                     <CardDescription>My complete order history.</CardDescription>
                 </CardHeader>
                 <CardContent>
-                    <Table>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead className="w-[100px]">ID</TableHead>
-                                <TableHead>Payment Status</TableHead>
-                                <TableHead>Payment Method</TableHead>
-                                <TableHead>Date Time</TableHead>
-                                <TableHead>Order Status</TableHead>
-                                <TableHead>Amount</TableHead>
-                                <TableHead className="text-right">Details</TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {orders.map((order: Order) => {
-                                return (
-                                    <TableRow key={order._id}>
-                                        <TableCell className="font-medium">{order._id}</TableCell>
-                                        <TableCell>{order.paymentStatus.toUpperCase()}</TableCell>
-                                        <TableCell>{order.paymentMode}</TableCell>
-                                        <TableCell>{order.createdAt}</TableCell>
-                                        <TableCell>
-                                            <Badge variant={'outline'}>
-                                                {order.orderStatus.toUpperCase()}
-                                            </Badge>
-                                        </TableCell>
-                                        {/* todo: make sure the total is grand total */}
-                                        <TableCell>₹{order.total}</TableCell>
-                                        <TableCell className="text-right">
-                                            <Link
-                                                href={`/order/${order._id}`}
-                                                className="underline text-primary">
-                                                More details
-                                            </Link>
-                                        </TableCell>
-                                    </TableRow>
-                                );
-                            })}
-                        </TableBody>
-                    </Table>
+                    {orders.length === 0 ? (
+                        'No orders yet.'
+                    ) : (
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[100px]">ID</TableHead>
+                                    <TableHead>Payment Status</TableHead>
+                                    <TableHead>Payment Method</TableHead>
+                                    <TableHead>Date Time</TableHead>
+                                    <TableHead>Order Status</TableHead>
+                                    <TableHead>Amount</TableHead>
+                                    <TableHead className="text-right">Details</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {orders.map((order: Order) => {
+                                    return (
+                                        <TableRow key={order._id}>
+                                            <TableCell className="font-medium">
+                                                {order._id}
+                                            </TableCell>
+                                            <TableCell>
+                                                {order.paymentStatus.toUpperCase()}
+                                            </TableCell>
+                                            <TableCell>{order.paymentMode}</TableCell>
+                                            <TableCell>{order.createdAt}</TableCell>
+                                            <TableCell>
+                                                <Badge variant={'outline'}>
+                                                    {order.orderStatus.toUpperCase()}
+                                                </Badge>
+                                            </TableCell>
+                                            {/* todo: make sure the total is grand total */}
+                                            <TableCell>₹{order.total}</TableCell>
+                                            <TableCell className="text-right">
+                                                <Link
+                                                    href={`/order/${order._id}`}
+                                                    className="underline text-primary">
+                                                    More details
+                                                </Link>
+                                            </TableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                            </TableBody>
+                        </Table>
+                    )}
                 </CardContent>
             </Card>
         </div>
