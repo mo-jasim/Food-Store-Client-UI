60c861d34f07a12b1830381b9aa27aa2896ac6fe
Rakesh K <codersgyan@gmail.com>
2024-07-04 18:31:12 +0300

    add orders link

--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -53,7 +53,7 @@ const Header = async () => {
                             </Link>
                         </li>
                         <li>
-                            <Link className="hover:text-primary" href={'/'}>
+                            <Link className="hover:text-primary" href={'/orders'}>
                                 Orders
                             </Link>
                         </li>
